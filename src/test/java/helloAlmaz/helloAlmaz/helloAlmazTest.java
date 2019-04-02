290155a453997322a1422d2b165cdeb844f9c34b
BIG BABY ALMAZ <47759026+15170016@users.noreply.github.com>
2019-04-02 19:39:46 +0500

    Update helloAlmazTest.java

--- src/test/java/helloAlmaz/helloAlmaz/helloAlmazTest.java
+++ src/test/java/helloAlmaz/helloAlmaz/helloAlmazTest.java
@@ -25,7 +25,7 @@ public class helloAlmazTest
      */
     public static Test suite()
     {
-        return new TestSuite( helloAlmazTest .class );
+        return new TestSuite( helloAlmazTest.class );
     }
 
     /**
