4b711307732ba61e023a50010b8f2be7a580cdcf
BIG BABY ALMAZ <47759026+15170016@users.noreply.github.com>
2019-04-02 19:24:06 +0500

    Update helloAlmazTest.java

--- src/test/java/helloAlmaz/helloAlmaz/helloAlmazTest.java
+++ src/test/java/helloAlmaz/helloAlmaz/helloAlmazTest.java
@@ -1,4 +1,4 @@
-package HHHello.HHHello;
+package helloAlmaz.helloAlmaz;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class helloAlmazTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public helloAlmazTest ( String testName )
     {
         super( testName );
     }
@@ -25,13 +25,13 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( helloAlmazTest .class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testhelloAlmaz ()
     {
         assertTrue( true );
     }
