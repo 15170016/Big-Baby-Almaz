76f3b40b26dc446982ffe66828e1313ab303fe48
BIG BABY ALMAZ <47759026+15170016@users.noreply.github.com>
2019-04-02 19:18:59 +0500

    Create helloAlmaz.java

---
+++ src/main/java/helloAlmaz/helloAlmaz.java
@@ -0,0 +1,13 @@
+package helloAlmaz.helloAlmaz;
+
+/**
+ * helloAlmaz!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hello Almaz!" );
+    }
+}
