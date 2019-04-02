1b799e07c15042cdb79845ca3eac288a4c6616cd
BIG BABY ALMAZ <47759026+15170016@users.noreply.github.com>
2019-04-02 21:39:58 +0500

    Update and rename src/main/java/helloAlmaz/helloAlmaz.java to src/main/java/HelloAlmaz/HelloAlmaz/HelloAlmaz.java

---
+++ src/main/java/HelloAlmaz/HelloAlmaz/HelloAlmaz.java
@@ -0,0 +1,13 @@
+package HelloAlmaz.HelloAlmaz;
+
+/**
+ * HelloAlmaz!
+ *
+ */
+public class HelloAlmaz
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Almaz!" );
+    }
+}
